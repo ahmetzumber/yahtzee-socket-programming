cf3c08614366e3e32455a0d9982f6232f9093b8e
Ahmet <ahmetzumber5@gmail.com>
2021-05-02 01:27:06 +0300

    calculate func added

--- src/Utility/DiceUtility.java
+++ src/Utility/DiceUtility.java
@@ -6,14 +6,60 @@
 package Utility;
 
 import java.util.Arrays;
+import Message.ScoreMessage.Scores;
 
 /**
  *
  * @author ahmetzumberoglu
  */
 public class DiceUtility {
-    
+
     // Three of a kind and four of a kind should be modified
+    public static int CALCULATE(Dice dices[], Scores score_type) {
+        int score = 0;
+        switch (score_type) {
+            case ONES:
+                score = calculateOnes(dices);
+                break;
+            case TWOS:
+                score = calculateTwos(dices);
+                break;
+            case THREES:
+                score = calculateThrees(dices);
+                break;
+            case FOURS:
+                score = calculateFours(dices);
+                break;
+            case FIVES:
+                score = calculateFives(dices);
+                break;
+            case SIXES:
+                score = calculateSixes(dices);
+                break;
+            case THREEKIND:
+                score = threeOfKind(dices);
+                break;
+            case FOURKIND:
+                score = fourOfKind(dices);
+                break;
+            case FULLHOUSE:
+                score = fullHouse(dices);
+                break;
+            case SMALLSTR:
+                score = smallStraight(dices);
+                break;
+            case LARGESTR:
+                score = largeStraight(dices);
+                break;
+            case CHANCE:
+                score = chance(dices);
+                break;
+            case YAHTZEE:
+                score = yahtzee(dices);
+                break;
+        }
+        return score;
+    }
 
     public static int calculateOnes(Dice dices[]) {
         int result = 0;
@@ -93,30 +139,55 @@ public class DiceUtility {
     public static int fourOfKind(Dice dices[]) {
         int ones = 0, twos = 0, threes = 0, fours = 0, fives = 0, sixes = 0;
         for (Dice d : dices) {
-            if (d.value == 1) ones++;   
-            if (d.value == 2) twos++;         
-            if (d.value == 3) threes++;      
-            if (d.value == 4) fours++;    
-            if (d.value == 5) fives++;    
-            if (d.value == 6) sixes++;          
+            if (d.value == 1) {
+                ones++;
+            }
+            if (d.value == 2) {
+                twos++;
+            }
+            if (d.value == 3) {
+                threes++;
+            }
+            if (d.value == 4) {
+                fours++;
+            }
+            if (d.value == 5) {
+                fives++;
+            }
+            if (d.value == 6) {
+                sixes++;
+            }
         }
         // if anyone has 4 same number than return 25 point
-        if (ones == 4 || twos == 4 || threes == 4 || fours == 4 || fives == 4 || sixes == 4) 
-            return 25;  
+        if (ones == 4 || twos == 4 || threes == 4 || fours == 4 || fives == 4 || sixes == 4) {
+            return 25;
+        }
         return 0;
     }
 
-    public static int fullHouse(Dice[] dices) {   
+    public static int fullHouse(Dice[] dices) {
         int ones = 0, twos = 0, threes = 0, fours = 0, fives = 0, sixes = 0;
         for (int i = 0; i < 5; i++) {
-            if (dices[i].value == 1) ones++;       
-            if (dices[i].value == 2) twos++;  
-            if (dices[i].value == 3) threes++;
-            if (dices[i].value == 4) fours++;
-            if (dices[i].value == 5) fives++;
-            if (dices[i].value == 6) sixes++;
+            if (dices[i].value == 1) {
+                ones++;
+            }
+            if (dices[i].value == 2) {
+                twos++;
+            }
+            if (dices[i].value == 3) {
+                threes++;
+            }
+            if (dices[i].value == 4) {
+                fours++;
+            }
+            if (dices[i].value == 5) {
+                fives++;
+            }
+            if (dices[i].value == 6) {
+                sixes++;
+            }
         }
-        if((ones == 3 && (twos == 2 || threes == 2 || fours == 2
+        if ((ones == 3 && (twos == 2 || threes == 2 || fours == 2
                 || fives == 2 || sixes == 2)) || (twos == 3 && (ones == 2
                 || threes == 2 || fours == 2 || fives == 2 || sixes == 2))
                 || (threes == 3 && (ones == 2 || twos == 2 || fours == 2
@@ -124,7 +195,7 @@ public class DiceUtility {
                 || threes == 2 || twos == 2 || fives == 2 || sixes == 2))
                 || (fives == 3 && (ones == 2 || threes == 2 || fours == 2
                 || twos == 2 || sixes == 2)) || (sixes == 3 && (ones == 2
-                || threes == 2 || fours == 2 || fives == 2 || twos == 2))){
+                || threes == 2 || fours == 2 || fives == 2 || twos == 2))) {
             return 25;
         }
         return 0;
@@ -133,12 +204,24 @@ public class DiceUtility {
     public static int smallStraight(Dice[] dices) {
         int ones = 0, twos = 0, threes = 0, fours = 0, fives = 0, sixes = 0;
         for (int i = 0; i < 5; i++) {
-            if (dices[i].value == 1) ones++;
-            if (dices[i].value == 2) twos++;   
-            if (dices[i].value == 3) threes++;   
-            if (dices[i].value == 4) fours++;  
-            if (dices[i].value == 5) fives++;  
-            if (dices[i].value == 6) sixes++;        
+            if (dices[i].value == 1) {
+                ones++;
+            }
+            if (dices[i].value == 2) {
+                twos++;
+            }
+            if (dices[i].value == 3) {
+                threes++;
+            }
+            if (dices[i].value == 4) {
+                fours++;
+            }
+            if (dices[i].value == 5) {
+                fives++;
+            }
+            if (dices[i].value == 6) {
+                sixes++;
+            }
         }
         // 3 different combination of 4
         if ((ones == 1 && twos == 1 && threes == 1 && fours == 1)
@@ -152,12 +235,24 @@ public class DiceUtility {
     public static int largeStraight(Dice[] dices) {
         int ones = 0, twos = 0, threes = 0, fours = 0, fives = 0, sixes = 0;
         for (int i = 0; i < dices.length; i++) {
-            if (dices[i].value == 1) ones++;           
-            if (dices[i].value == 2) twos++;          
-            if (dices[i].value == 3) threes++;           
-            if (dices[i].value == 4) fours++;           
-            if (dices[i].value == 5) fives++;           
-            if (dices[i].value == 6) sixes++;    
+            if (dices[i].value == 1) {
+                ones++;
+            }
+            if (dices[i].value == 2) {
+                twos++;
+            }
+            if (dices[i].value == 3) {
+                threes++;
+            }
+            if (dices[i].value == 4) {
+                fours++;
+            }
+            if (dices[i].value == 5) {
+                fives++;
+            }
+            if (dices[i].value == 6) {
+                sixes++;
+            }
         }
         // 2 different combination of 5
         if ((ones == 1 && twos == 1 && threes == 1 && fours == 1 && fives == 1)
@@ -169,25 +264,38 @@ public class DiceUtility {
 
     public static int chance(Dice[] dice) {
         int result = 0;
-        for (Dice d : dice) 
-            result += d.value;      
+        for (Dice d : dice) {
+            result += d.value;
+        }
         return result;
     }
 
-    public static int yahtzee(Dice dices[]){
+    public static int yahtzee(Dice dices[]) {
         int ones = 0, twos = 0, threes = 0, fours = 0, fives = 0, sixes = 0;
         for (int i = 0; i < dices.length; i++) {
-            if (dices[i].value == 1) ones++; 
-            if (dices[i].value == 2) twos++;
-            if (dices[i].value == 3) threes++;    
-            if (dices[i].value == 4) fours++;         
-            if (dices[i].value == 5) fives++;  
-            if (dices[i].value == 6) sixes++;
+            if (dices[i].value == 1) {
+                ones++;
+            }
+            if (dices[i].value == 2) {
+                twos++;
+            }
+            if (dices[i].value == 3) {
+                threes++;
+            }
+            if (dices[i].value == 4) {
+                fours++;
+            }
+            if (dices[i].value == 5) {
+                fives++;
+            }
+            if (dices[i].value == 6) {
+                sixes++;
+            }
         }
-        if (ones == 5 || twos == 5 || threes == 5 || fours == 5 || fives == 5 || sixes == 5)
-            return 50;      
+        if (ones == 5 || twos == 5 || threes == 5 || fours == 5 || fives == 5 || sixes == 5) {
+            return 50;
+        }
         return 0;
     }
 
-    
 }
